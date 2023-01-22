4092257c26ee8b7acd74cdc5c56a0bb1dd5b758f
Symbytex <cagrisntrk0614@gmail.com>
2023-01-23 02:00:44 +0300

    Add project files.

---
+++ BussinesLayer/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using BussinesLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactdal;
+
+        public ContactManager(IContactDal contactdal)
+        {
+            this._contactdal = contactdal;
+        }
+
+        public void TAdd(Contact t)
+        {
+            _contactdal.Insert(t);
+        }
+
+        public void TDelete(Contact t)
+        {
+            _contactdal.Delete(t);
+        }
+
+        public Contact TGetByID(int id)
+        {
+          return  _contactdal.GetByID(id);
+        }
+
+        public List<Contact> TGetList()
+        {
+           return _contactdal.GetList();
+        }
+
+        public void TUpdate(Contact t)
+        {
+            _contactdal.Update(t);
+        }
+    }
+}
---
+++ Core_project/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,23 @@
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_project.ViewComponents.Feature
+{
+    public class FeatureList:ViewComponent
+    {
+        FeatureManager featuremanager = new FeatureManager(new EfFeatureDal());
+
+        //component'in en büyük avantajı,partialda ki gibi direkt erişim yok.Yani /Default/FeatureList yazdığınızda erişilmiyor.
+        public IViewComponentResult Invoke()
+        {
+
+            var values = featuremanager.TGetList();
+            return View(values);
+        }
+    }
+}
