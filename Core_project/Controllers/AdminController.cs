c26d7abfc4ff30418f4fbc4bf072729013b4e765
Symbytex <cagrisntrk0614@gmail.com>
2023-01-30 20:52:49 +0300

    .

---
+++ Core_project/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_project.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
