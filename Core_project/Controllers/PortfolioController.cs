5fa5a2a2821a44c0f34f518f8f50298b25965331
Symbytex <cagrisntrk0614@gmail.com>
2023-02-14 02:25:49 +0300

    .

---
+++ Core_project/Controllers/PortfolioController.cs
@@ -0,0 +1,18 @@
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_project.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            var values=portfolioManager.TGetList();
+            return View(values);
+        }
+
+
+    }
+}
