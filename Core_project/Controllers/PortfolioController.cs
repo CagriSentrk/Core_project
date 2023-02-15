55b8034148a90e2c11aec7f9a541bad640a9b9e6
Symbytex <cagrisntrk0614@gmail.com>
2023-02-16 00:17:24 +0300

    .

--- Core_project/Controllers/PortfolioController.cs
+++ Core_project/Controllers/PortfolioController.cs
@@ -1,6 +1,8 @@
 ﻿using BussinesLayer.Concrete;
+using BussinesLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_project.Controllers
@@ -16,21 +18,78 @@ namespace Core_project.Controllers
 
 
 
-            var values=portfolioManager.TGetList();
+            var values = portfolioManager.TGetList();
             return View(values);
         }
         [HttpGet]
-         public IActionResult AddPortfolio()
+        public IActionResult AddPortfolio()
         {
             return View();
         }
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio p)
         {
-            portfolioManager.TAdd(p);
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(p);
 
+            if (results.IsValid)
+            {
+                portfolioManager.TAdd(p);
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+
+        }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetByID(id);
+            portfolioManager.TDelete(values);
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var values = portfolioManager.TGetByID(id);
+
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio p)
+        {
+            PortfolioValidator validators = new PortfolioValidator();    
+       ValidationResult result = validators.Validate(p);
+            if (result.IsValid)
+            {
+                portfolioManager.TUpdate(p);
+
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+
+                }
+                return View();
+
+            }
+
+        }
+
+
 
 
 
