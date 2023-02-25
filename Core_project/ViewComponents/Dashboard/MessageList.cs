047cc14dd46041194968ff6ad9de13712526f7df
Symbytex <cagrisntrk0614@gmail.com>
2023-02-26 01:51:07 +0300

    .

---
+++ Core_project/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_project.ViewComponents.Dashboard
+{
+    public class MessageList:ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
